2843f9e0c704657f3864c204cc0e489de2c1df6c
kamila <kamila.machado@cs.cruzeirodosul.edu.br>
2025-04-18 16:31:24 -0300

    modo escuro

--- script.js
+++ script.js
@@ -55,3 +55,18 @@ document.addEventListener('DOMContentLoaded',() => {
 
     displayIdols(idols);
 })
+
+const toggleBtn = document.getElementById("toggle-theme");
+
+const currentTheme = localStorage.getItem("theme");
+if(currentTheme === "dark"){
+    document.body.classList.add("dark");
+    toggleBtn.textContent = "☀️";
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    const isDark = document.body.classList.contains("dark");
+    toggleBtn.textContent = isDark ? "☀️" : "🌙";
+    localStorage.setItem("theme", isDark ? "dark" : "light")
+});
